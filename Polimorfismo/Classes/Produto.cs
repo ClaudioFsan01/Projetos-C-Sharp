af607a8a121d321120b648a58de33356a43a1e6c
Claudio Felix <claudio.felixdossantos@yahoo.com.br>
2020-06-23 19:20:00 -0300

    Polimorfismo, Classes e métodos abstratos

---
+++ Polimorfismo/Classes/Produto.cs
@@ -0,0 +1,152 @@
+﻿/*Classes abstratas
+• São classes que não podem ser instanciadas
+• É uma forma de garantir herança total: somente subclasses não
+abstratas podem ser instanciadas, mas nunca a superclasse abstrata.
+
+ Dessa forma, não podemos mais criar objetos do tipo Produto , mas podemos ainda usar variáveis do
+tipo Produto, para referenciar objetos de outros tipos.
+
+ Métodos abstratos 
+
+ Podemos obrigar todas as classes filhas a sobrescreverem um método da classe mãe, basta declarar
+ esse método com o modificador abstract na classe mãe(super classe) ao invés de virtual . Toda vez que marcamos um método
+ com o modificador abstract , ele obrigatoriamente não pode ter uma implementação padrão:
+
+ Podemos ter uma classe abstrata sem nenhum método abstrato, mas não o contrário. Se a classe tem
+pelo menos um método abstrato, ela deve ser abstrata também pois como o método está incompleto, a
+classe não está completa.
+ 
+ */
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Polimorfismo
+{
+    public abstract class Produto // classe abstrata Produto não pode ser instanciada, somente suas classes filhas não abstratas.
+    {
+             
+       
+        private protected string _nome;  // Modificador de acesso private protected permite que a propria classe e suas subclasses(no mesmo projeto) acessem o atributo
+       
+        public decimal Preco { get; private protected set; }
+        
+        public int QtdEstoque { get; private protected set; }
+
+    
+
+        public Produto() //Construtor padrão
+        {
+            
+            QtdEstoque = 0;
+        }
+
+        public Produto(decimal preco) : this() 
+        {
+            //_preco = preco;
+            if(preco > 0)
+            {
+                Preco = preco;
+            }
+            else
+            {
+                Console.WriteLine("Preço invalido ! \n");
+            }
+          
+        }
+     
+     
+       public Produto(decimal preco, int qtdEstoque) : this(preco)
+       {
+            if(qtdEstoque >= 0)
+            {
+                          
+                QtdEstoque = qtdEstoque;
+            }
+            else
+            {
+                Console.WriteLine(" Preco ou QtdEstoque com valor invalido ! ");
+            }
+
+       }
+                
+        // Properties customizada
+
+            public String Nome
+            {
+             get { return _nome; }
+              set
+              {
+                if(value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+                else
+                {
+                    Console.WriteLine(" Nome invalido !");
+                }
+
+ 
+              }
+            }
+
+
+
+        //public double Preco { get { return _preco; } }
+
+
+
+        // public int QtdEmEstoque { get { return _qtdEstoque; } }
+
+
+        //Sobreposição ou sobrescrita
+        //• É a implementação de um método de uma superclasse na subclasse
+        /*Para que um método comum (não
+        abstrato) possa ser sobreposto, deve
+        ser incluído nele o prefixo "virtual.
+        
+        A palavra ( virtual ) em um método na super classe (classe pai) indica que o
+        método pode ser sobreposto ou sobrescrito na subclasse (classe filha) com o uso da palavra override. "*/
+
+       
+
+        public abstract decimal PrecoProdutoComTaxa();  /*método com modificador abstract obriga todas as classes filhas da classe Produto
+                                                       a sobrescreverem este método da classe mãe.
+                                                       Toda vez que marcamos um método com o modificador abstract , ele obrigatoriamente não
+                                                       pode ter uma implementação padrão.*/
+
+
+        public virtual decimal ValorTotalEmEstoque()
+        {
+        return Preco * QtdEstoque;
+        }
+
+       public void AdicionarProdutos(int quantidade)
+       {
+            QtdEstoque += quantidade;
+       }
+
+       public void RemoverProdutos(int quantidade)
+       {
+         if (quantidade <= QtdEstoque)
+         {
+                QtdEstoque -= quantidade;
+         }
+         else
+         {
+            Console.WriteLine(" Quantidade no estoque menor que a solicitada ! \n");
+         }
+
+  
+       }
+
+
+       
+}
+}
---
+++ Polimorfismo/Classes/ProdutoImportado.cs
@@ -0,0 +1,105 @@
+﻿
+/* Classes e métodos selados
+ • Palavra chave: sealed
+
+ • Classe: evita que a classe seja herdada
+ • Nota: ainda é possível extender a funcionalidade de uma classe selada usando
+ "extension methods"
+ 
+ • Método: evita que um método sobreposto(reescrito) possa ser sobreposto(reescrito) novamente
+• Só pode ser aplicado a métodos sobrepostos(reescritos)
+ 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Polimorfismo
+{// com o uso da palavra sealed a classe ProdutoImportado não pode ser herdada 
+    /*sealed*/ class ProdutoImportado : Produto    // Basta usar a notação : e o C# automaticamente herda os métodos e atributos da classe pai.
+    {
+        private protected int _codprodutoimportado;
+
+        public decimal ImpostoImportacao { get; private set; }
+
+        public ProdutoImportado(decimal preco, int qtdEstoque, decimal impostoImportacao) : base(preco, qtdEstoque)
+        {
+            if (impostoImportacao >= 0)
+            {
+                ImpostoImportacao = impostoImportacao;
+            }
+            else
+            {
+                throw new ArgumentException(" Valor da taxa de importação invalida ! \n");
+            }
+          
+        }
+
+
+        //propriedade customizada 
+
+        public int CodigoProdImportado {
+            get { return _codprodutoimportado; }
+            set
+            {
+                if(value > 0)
+                {
+                    _codprodutoimportado = value;
+                }
+                else
+                {
+                    Console.WriteLine(" Codigo do produto nacional inválido !");
+                }
+            }
+        }
+
+        /* Desejo que o método ValorTotalEmEstoque() não seja sobreposto(reescrito) novamente por isso o uso da palavra sealed.
+        Pra quê?
+        • Segurança: dependendo das regras do negócio, às vezes é desejável garantir que uma classe não seja herdada, ou que um método não
+        seja sobreposto novamente.
+        • Geralmente convém selar métodos sobrepostos, pois sobreposições múltiplas podem ser uma porta de entrada para inconsistências
+         • Performance: atributos de tipo de uma classe selada são analisados
+         de forma mais rápida em tempo de execução.
+         • Exemplo clássico: string         
+         */
+        public sealed override decimal ValorTotalEmEstoque()
+        {
+            return PrecoProdutoComTaxa() * QtdEstoque;
+        }
+
+        public override decimal PrecoProdutoComTaxa()
+        {
+           
+            return (Preco * 0.15M) + (Preco*ImpostoImportacao/100) + Preco;
+            // (base.PrecoProdutoComTaxa()*0.15) + ImpostoImportacao +base.PrecoProdutoComTaxa();
+        }
+
+
+        /* Método ToString() na classe Object é virtual podendo ser (sobreposto ou reescrito) 
+         em outras classes utilizando a palavra override  */
+
+        public override string ToString() //Retorna uma cadeira de caracteres que representa o objeto atual 
+        {//Aqui definimos como o objeto será retornado na forma de String
+            return " Codigo do Produto importado : " + _codprodutoimportado +
+                "- Nome : " + _nome +
+                "- Preço sem a taxa : $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) +
+                "- Taxa de Imposto de importação :  " + ImpostoImportacao.ToString(CultureInfo.InvariantCulture) +" % "+
+                "- Preço com a taxa : $ " + PrecoProdutoComTaxa().ToString("F2", CultureInfo.InvariantCulture) +
+               "- Quantidade em estoque : " + QtdEstoque +
+               "- Valor total em estoque : $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+        /* * ToString - converte um objeto para String  
+        * Utilizando o método ToString podemos escolher como o objeto será representado na forma de String*/
+
+
+
+
+    }
+}
